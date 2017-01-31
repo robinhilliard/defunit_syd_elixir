3d2505aa870a10dd03332e3b983866fad6dfb0fc
Robin Hilliard <robin@rocketboots.com>
2017-02-01 10:58:33 +1100

    add doc tags and exdoc

--- lib/defunit_talk.ex
+++ lib/defunit_talk.ex
@@ -1,48 +1,65 @@
 defmodule DefUnitTalk do
+  @moduledoc """
+  Talk given at Elixir Sydney Meetup.
+  """
 
+  @typedoc "SI weight"
   @type kg :: float
-  @type kgm3 :: float
-  @type m3 :: float
+  @typedoc "SI pressure"
+  @type kgm2 :: float
+  @typedoc "SI area"
+  @type m2 :: float
+  @typedoc "SI velocity"
   @type ms :: float
+  @typedoc "SI acceleration"
   @type ms2 :: float
+  @typedoc "pressure altitude"
   @type feet :: float
   
+  @typedoc "FPS weight"
   @type lbs :: float
+  @typedoc "Navigation velocity"
   @type knots :: float
   
+  @doc "Convert to core unit"
   @spec number <~ :kg :: kg
   def value <~ :kg do
     value
   end
   
-  @spec number <~ :m3 :: m3
-  def value <~ :m3 do
+  @doc "Convert to core unit"
+  @spec number <~ :m2 :: m2
+  def value <~ :m2 do
     value
   end
   
+  @doc "Convert to core unit"
   @spec number <~ :feet :: feet
   def value <~ :feet do
     value
   end
   
+  @doc "Convert to core unit"
   @spec number <~ :lbs :: kg
   def value <~ :lbs do
     value / 2.20
   end
   
+  @doc "Convert to other unit"
   @spec ms ~> :knots :: knots
   def value ~> :knots do
     value * 1.94
   end
   
 
-  
+  @doc "Acceleration due to gravity on Earth"
   @spec g() :: ms2
   def g() do
     9.81
   end
   
-  @spec p(feet) :: kgm3
+  @doc "Pressure at altitude"
+  @spec p(feet) :: kgm2
   def p(alt) do
     8.0e-19 * :math.pow(alt, 4)  \
     - 4.0e-14 * :math.pow(alt, 3) \
@@ -51,16 +68,18 @@ defmodule DefUnitTalk do
     + 1.225
   end
 
-  @spec stall_speed(kg, m3, float, feet) :: ms
+  @doc "Stall speed"
+  @spec stall_speed(kg, m2, float, feet) :: ms
   def stall_speed(mass, wing_area, lift_coefficient, altitude) do
     :math.sqrt((2.0 * mass * g()) /
     (p(altitude) * wing_area * lift_coefficient))
   end
   
+  @doc "Piper Archer stall speed"
   @spec piper_archer_stall_speed_knots() :: knots
   def piper_archer_stall_speed_knots() do
     mass = 2545 <~ :lbs
-    wing_area = 15.8 <~ :m3
+    wing_area = 15.8 <~ :m2
     coefficient_of_lift = 2.1
     altitude = 0 <~ :feet
     stall_speed(mass, wing_area, coefficient_of_lift, altitude) ~> :knots
